d5ec96b23e0c62730cb67552948811ff5a0ac458
vinai <vinaibangala2001@gmail.com>
2020-01-14 06:11:53 +0530

    64 bit compilation and call sys_exit at end

--- pgm/01_hello_world/pgm.asm
+++ pgm/01_hello_world/pgm.asm
@@ -9,6 +9,12 @@ _start:
 						;(line feed character)
 	mov ecx,msg			; move the memory address of our message string into ecx
 	mov ebx,1			; write to the stdout file
-	mov eax,4			;invoke SYS_WRITE (kernel opcode 4)
+	mov eax,4			;invoke (kernel opcode 4) SYS_WRITE (
+						;uint_32 fd,				//ebx
+						;const char __user *buf, 	//ecx
+						;size_t count)				// edx
 	int 80h
 
+	mov ebx,0			; argument 0 for sys_exit, indicates success
+	mov eax,1			; sys_exit (kernel opcode 1)
+	int 80h
