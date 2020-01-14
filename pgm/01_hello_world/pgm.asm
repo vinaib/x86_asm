a8436ed722a84608921a3249b54505da5263c51d
vinai <vinaibangala2001@gmail.com>
2020-01-14 08:34:36 +0530

    identation

--- pgm/01_hello_world/pgm.asm
+++ pgm/01_hello_world/pgm.asm
@@ -1,20 +1,20 @@
-section .data
+		section .data
 msg		db		'Hello world!',0Ah	;assign msg variable with your message
 
-section .text
-global _start
+		section .text
+		global _start
 
 _start:
-	mov edx,13			;number of bytes to write - one for each letter plus 0Ah
-						;(line feed character)
-	mov ecx,msg			; move the memory address of our message string into ecx
-	mov ebx,1			; write to the stdout file
-	mov eax,4			;invoke (kernel opcode 4) SYS_WRITE (
-						;uint_32 fd,				//ebx
-						;const char __user *buf, 	//ecx
-						;size_t count)				// edx
-	int 80h
+		mov edx,13			;number of bytes to write - one for each letter plus 0Ah
+							;(line feed character)
+		mov ecx,msg			; move the memory address of our message string into ecx
+		mov ebx,1			; write to the stdout file
+		mov eax,4			;invoke (kernel opcode 4) SYS_WRITE (
+							;uint_32 fd,				//ebx
+							;const char __user *buf, 	//ecx
+							;size_t count)				// edx
+		int 80h
 
-	mov ebx,0			; argument 0 for sys_exit, indicates success
-	mov eax,1			; sys_exit (kernel opcode 1)
-	int 80h
+		mov ebx,0			; argument 0 for sys_exit, indicates success
+		mov eax,1			; sys_exit (kernel opcode 1)
+		int 80h
